95cb08ee6520752991a9ba524daad2012fdd2df8
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2025-10-21 15:21:22 +0200

    github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://krist0ff-t.github.io/astro_blog_tutorial',
+});
